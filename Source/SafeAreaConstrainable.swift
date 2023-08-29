9c96738df9cb1f982c15be66130feac97b2fc254
Wavin Dev <eshwavin@gmail.com>
2023-08-29 15:15:00 -0400

    Added Classes

---
+++ Source/SafeAreaConstrainable.swift
@@ -0,0 +1,13 @@
+//
+//  SafeAreaConstrainable.swift
+//  ConstraintKit
+//
+//  Created by Srivinayak Chaitanya Eshwa on 28/08/23.
+//
+
+import UIKit
+
+public protocol SafeAreaConstrainable: Constrainable {
+    var safeAreaLayoutGuide: UILayoutGuide { get }
+    var translatesAutoresizingMaskIntoConstraints: Bool { get set }
+}
---
+++ Source/UIView+Constrainable.swift
@@ -0,0 +1,30 @@
+//
+//  UIView+Constrainable.swift
+//  ConstraintKit
+//
+//  Created by Srivinayak Chaitanya Eshwa on 28/08/23.
+//
+
+import UIKit
+
+extension UIView: Constrainable {
+    public var container: Constrainable? {
+        return superview
+    }
+    
+    public var optionalSafeAreaLayoutGuide: UILayoutGuide? {
+        return safeAreaLayoutGuide
+    }
+}
+
+public extension UIView {
+    func addConstrainable(_ constrainable: Constrainable) {
+        if let view = constrainable as? UIView {
+            addSubview(view)
+        }
+        else if let layoutGuide = constrainable as? UILayoutGuide {
+            addLayoutGuide(layoutGuide)
+        }
+    }
+}
+
